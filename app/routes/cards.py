c109fa26c84a11e8dc23b2d433a2bbc0becfa730
Tijmen <tpm@maplemail.nl>
2026-03-15 16:07:05 +0100

    Setup HelloWorld

--- app/routes/cards.py
+++ app/routes/cards.py
@@ -1,3 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 cards_bp = Blueprint("cards", __name__)
+
+
+@cards_bp.route("/")
+def index():
+    """Render the home page."""
+    return render_template("cards/index.html")
