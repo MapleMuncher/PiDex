77c601583b28fa66c29d80a16d26bd17ef4e3a9f
Tijmen <tpm@maplemail.nl>
2026-03-18 15:06:22 +0100

    Add single card view

--- app/routes/sets.py
+++ app/routes/sets.py
@@ -5,5 +5,9 @@ sets_bp = Blueprint("sets", __name__)
 
 @sets_bp.route("/sets")
 def index():
-    """Render the home page."""
     return render_template("sets/index.html")
+ 
+ 
+@sets_bp.route("/<set_id>")
+def detail(set_id):
+    return render_template("sets/detail.html", set_id=set_id)
